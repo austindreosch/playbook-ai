fe9e0ceb67cb87dc1e69802e92dd36842a1d0c02
Austin Dreosch <austindreosch@gmail.com>
2023-08-09 00:37:18 -0700

    getting all league data in one object by import

--- components/test/TestBlock.js
+++ components/test/TestBlock.js
@@ -1,15 +1,63 @@
 'use client'
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getLeagueData } from 'utilities/fantraxAPI'; // Import your API functions
 
 const TestBlock = () => {
+  const [leagueData, setLeagueData] = useState(null);
 
+  useEffect(() => {
+    // Call your API function to fetch the league data
+    const fetchData = async () => {
+      const leagueId = "4fzl7g0gljax6594"; // Replace with your actual league ID
+      const userTeamId = "u6v64wyeljax65ae"; // Replace with your actual user team ID
+      const leagueName = "Franchise Mode"; // Replace with your actual league name
+
+      const data = await getLeagueData(leagueId, userTeamId, leagueName);
+      setLeagueData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!leagueData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="">
+      {/* Display the league data */}
+      <h1>League Data</h1>
+      <p>League ID: {leagueData.leagueId}</p>
+      <p>User Team ID: {leagueData.userTeamId}</p>
+      <p>League Name: {leagueData.leagueName}</p>
 
+      {/* Display teams and their players */}
+      <h2>Teams and Players</h2>
+      {leagueData.teams.map((team) => (
+        <div key={team.teamId}>
+          <b>{team.teamName}</b>
+          <ul>
+            {team.players.map((player) => (
+              <li key={player.fantraxId}>
+                {player.name} - {player.position} - {player.team}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+
+      {/* Display waiver players */}
+      <b>Waiver Players</b>
+      <ul>
+        {leagueData.waiverPlayers.map((player) => (
+          <li key={player.fantraxId}>
+            {player.name} - {player.position} - {player.team}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default TestBlock;
--- utilities/fantraxAPI.js
+++ utilities/fantraxAPI.js
@@ -1,141 +1,174 @@
 /**
- * Retrieves teams and players data for a given league from Fantrax API.
+ * Retrieves comprehensive league data for a given league from the Fantrax API.
  *
  * @param {string} leagueId - The unique identifier of the individual league.
- * @returns {Promise<Object>} A Promise that resolves to an object containing the processed data. Processed data structure:
+ * @returns {Promise<Object|null>} A Promise that resolves to an object containing the processed league data, structured as follows:
  * 
- *                           { teamId: {
- *                               teamName: string,
- *                               players: [
- *                                 {
- *                                   fantraxId: string,
- *                                   name: string,
- *                                   position: string,
- *                                   team: string,
- *                                   statsIncId: number,
- *                                   rotowireId: number,
- *                                 },
- *                               ],
- *                             },
- *                            }
+ * {
+ *   leagueId: string,
+ *   teamId: string,
+ *   userTeamId: string,
+ *   userTeamName: string,
+ *   teams: [
+ *     {
+ *       teamId: string,
+ *       teamName: string,
+ *       players: [
+ *         {
+ *           fantraxId: string,
+ *           name: string,
+ *           position: string,
+ *           team: string,
+ *           rotowireId: number,
+ *         }
+ *       ]
+ *     }
+ *   ],
+ *   waiverPlayers: [
+ *     {
+ *       fantraxId: string,
+ *       name: string,
+ *       position: string,
+ *       team: string,
+ *       rotowireId: number,
+ *     }
+ *   ]
+ * }
+ * 
+ * The structure represents league information, including each team's players and a list of waiver players.
  * 
  * @throws {Error} If there is an error during the data retrieval or processing.
  */
 
 import axios from 'axios';
 
-// get all teams along with their players name and information
-export const getTeamsWithPlayers = async (leagueId) => {
+export const getLeagueData = async (leagueId, userTeamId, leagueName) => {
   const retrieveRostersURL = `https://www.fantrax.com/fxea/general/getTeamRosters?leagueId=${leagueId}`;
-  const playerDataURL = "https://www.fantrax.com/fxea/general/getPlayerIds?sport=NBA";
-
+  
   try {
-    // Fetch both rosters and player data in parallel for improved efficiency
-    const [rostersResponse, playerDataResponse] = await Promise.all([
-      axios.get(retrieveRostersURL),
-      axios.get(playerDataURL)
-    ]);
+    const rostersResponse = await axios.get(retrieveRostersURL);
     const rostersData = rostersResponse.data;
-    const playerDataMap = playerDataResponse.data;
 
-    const processedData = {};
+    const leagueData = {
+      leagueId: leagueId,
+      userTeamId: userTeamId,
+      leagueName: leagueName,
+      leagueInfo: {},
+      teams: [],
+      waiverPlayers: []
+    };
 
-    // Process data for each team and their respective players
+    leagueData.waiverPlayers = await getWaivers(leagueId);
+    leagueData.leagueInfo = await getLeagueInfo(leagueId);
+
     for (let teamId in rostersData) {
       const teamData = rostersData[teamId];
       const players = [];
 
-      // Loop through each player in the team's roster
+
+      console.log(teamData.rosterInfo[0]);
+      // If rosterItems contains player details directly:
       teamData.rosterItems.forEach(item => {
-        const playerData = playerDataMap[item.id];
-        if (playerData) {
-          // Create a simplified player object and add it to the players array
-          players.push({
-            fantraxId: playerData.fantraxId,
-            name: playerData.name,
-            position: playerData.position,
-            team: playerData.team,
-            statsIncId: playerData.statsIncId,
-            rotowireId: playerData.rotowireId,
-          });
-        }
+        players.push({
+          fantraxId: item.fantraxId,
+          name: item.name,
+          position: item.position,
+          team: item.team,
+          rotowireId: item.rotowireId,
+        });
       });
 
-      // Store the team data with its players in the processedData object
-      processedData[teamId] = {
+      leagueData.teams.push({
+        teamId: teamId,
         teamName: teamData.teamName,
         players: players
-      };
+      });
     }
 
-    console.log(processedData);
-    return processedData;
+    console.log(leagueData);
+    return leagueData;
 
   } catch (error) {
     console.error("Error fetching and processing league data:", error);
+    throw error;
+  }
+}
+
+
+
+
+
+// get all players then sort out inactive players, for a waiver list
+export const getWaivers = async (leagueId) => {
+  const playerDataURL = "https://www.fantrax.com/fxea/general/getPlayerIds?sport=NBA";
+  
+  try {
+    const playerDataResponse = await axios.get(playerDataURL);
+    const playerDataMap = playerDataResponse.data;
+
+    const waiverPlayers = [];
+
+    for (let playerId in playerDataMap) {
+      const playerDataItem = playerDataMap[playerId]; // Change variable name to avoid conflict
+      if (playerDataItem.team !== "(N/A)") {
+        waiverPlayers.push({
+          fantraxId: playerDataItem.fantraxId,
+          name: playerDataItem.name,
+          position: playerDataItem.position,
+          team: playerDataItem.team,
+          rotowireId: playerDataItem.rotowireId,
+        });
+      }
+    }
+
+    return waiverPlayers;
+
+  } catch (error) {
+    console.error("Error fetching waiver player data:", error);
     return null;
   }
 }
 
 
 
+// get roster size and active roster size, scoring system, and later on get matchups etc
+export const getLeagueInfo = async (leagueId) => {
+  const leagueInfoUrl = `https://www.fantrax.com/fxea/general/getLeagueInfo?leagueId=${leagueId}`
+  
+  try {
+    const leagueInfoResponse = await axios.get(leagueInfoUrl);
+    const leagueInfoData = leagueInfoResponse.data;
 
+    // console.log(leagueInfoData);
+    const positionConstraints = leagueInfoData.rosterInfo.positionConstraints;
+    const positionArray = generatePositionArray(positionConstraints);
 
-// get all players in the league
-export const getAllPlayers = async (leagueId) => {
+    const leagueInfo = {
+      activeRosterSize: leagueInfoData.rosterInfo.maxTotalActivePlayers,
+      totalRosterSize: leagueInfoData.rosterInfo.maxTotalPlayers,
+      positionArray: positionArray
+    };
 
-}
+    return leagueInfo;
 
-
-
-{ leagueInfo: {
-    leagueId: string,
-    teams: [
-      teamId1: {
-        teamName: string,
-        players: [
-          {
-            teamName: string,
-            players: [
-              {
-                fantraxId: string,
-                name: string,
-                position: string,
-                team: string,
-                rotowireId: number,
-              }
-            ]
-          }
-        ]
-      }
-    ],
-    waiverPlayers: [
-      // enter each player that isnt on a roster, and doesnt have N/A for their NBA team
-      {
-        fantraxId: string,
-        name: string,
-        position: string,
-        team: string,
-        rotowireId: number,
-      }
-    ]
+  } catch (error) {
+    console.error("Error fetching waiver player data:", error);
+    return null;
   }
 }
-  
 
 
+// helper function to generate position array for getLeagueInfo
+const generatePositionArray = (positionConstraints) => {
+  const positionArray = [];
   
-//   teamId: {
-//     teamName: string,
-//     players: [
-//       {
-//         fantraxId: string,
-//         name: string,
-//         position: string,
-//         team: string,
-//         statsIncId: number,
-//         rotowireId: number,
-//       },
-//     ],
-//   },
-// }
+  for (const position in positionConstraints) {
+    const maxActive = positionConstraints[position].maxActive;
+    
+    for (let i = 0; i < maxActive; i++) {
+      positionArray.push(position);
+    }
+  }
+  
+  return positionArray;
+};
