d1cf407550062a95cd17a3e3674b0122fbbdf609
Austin Dreosch <austindreosch@gmail.com>
2023-08-09 23:57:16 -0700

    finished connection with auth0

--- app/blog/[id]/page.js
+++
@@ -1,3 +0,0 @@
-export default function Page({params}){    
-    return <h1>Blog Page for ID: {params.id}</h1>
-}
---
+++ components/NavBar.js
@@ -0,0 +1,53 @@
+
+import Link from 'next/link';
+import React from 'react';
+
+
+import { Sen } from 'next/font/google';
+const oldFont = Sen({
+  weight: '700',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+import { Oxanium } from 'next/font/google';
+const logoFont = Oxanium({
+  weight: '600',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+
+function NavBar (){
+
+    return (
+        <nav className="bg-myyelloworange shadow-md">
+        <div className="max-w-[83rem] mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <a href="#" className="text-black hover:text-white px-3 py-2">
+                <div className={`${logoFont.className} text-3xl font-bold`}>playbook</div>
+              </a>
+              <span className={`${oldFont.className} text-sm font-bold mt-1`}> fantasy sports toolkit</span>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a href="#" className={`text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium ${oldFont.className}`}>Home</a>
+                <a href="#" className={`text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium ${oldFont.className}`}>Features</a>
+                <a href="#" className={`text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium ${oldFont.className}`}>Pricing</a>
+                <Link href="/about" className={`text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium ${oldFont.className}`}>About</Link>
+                <a href="#" className={`text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium ${oldFont.className}`}>Contact</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+}
+
+export default NavBar;
+
+
+
+
+
