0678c0ff8b146a0258d7f7bee1fb59045c83997d
Austin Dreosch <austindreosch@gmail.com>
2023-08-11 00:39:23 -0700

    styling rosterblock
    styling
    
    Update RosterBlock.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,8 @@ module.exports = {
         14: 'repeat(14, minmax(0, 1fr))',
         15: 'repeat(15, minmax(0, 1fr))',
         16: 'repeat(16, minmax(0, 1fr))',
+        17: 'repeat(17, minmax(0, 1fr))',
+        18: 'repeat(18, minmax(0, 1fr))',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
